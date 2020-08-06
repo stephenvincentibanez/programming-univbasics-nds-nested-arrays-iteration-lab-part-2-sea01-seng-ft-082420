473c5a71f29e73c3a46270e8d23855db80e12d1e
Stephen Ibanez <stephenvincentibanez@gmail.com>
2020-08-06 23:53:24 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,10 +5,11 @@ min_array=[]
 row_index=0 
 while row_index < src.count do 
   element_index=0 
-  min_element="".to_i
+  min_element=nil
   while element_index < src[row_index].count do 
-    if src[row_index][element_index]<min_element
-      src[row_index][element_index]=min_element
+    if min_element==nil ||
+      src[row_index][element_index]<min_element
+      min_element=src[row_index][element_index]
     end 
     element_index+=1
   end
